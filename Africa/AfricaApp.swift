6c06d39be95460026a3088ce6301ff0761dade05
saiashish9 <saiashish3760@gmail.com>
2022-02-17 13:41:36 +0530

    initial commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Sai Ashish Darapureddy on 17/02/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
